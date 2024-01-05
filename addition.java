0724a222c832d16f813eadd17bfc7477ec14efaa
pushparajdev05 <pushparajdev05@gmail.com>
2024-01-06 00:15:19 +0530

    the contex file changed into java addtion file.

---
+++ addition.java
@@ -0,0 +1,11 @@
+class addition
+{ 
+    public void main(String args[])
+    {
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        print("Addition of a and b is :"+c);
+    } 
+}
